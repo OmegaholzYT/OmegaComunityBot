1935784f190fa5c1087f90d596229072c935a568
OmegaholzYT <36573300+OmegaholzYT@users.noreply.github.com>
2018-02-18 10:37:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,34 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
+    if (message.content === 'Help') {
+    	message.reply('Meine Commands sind
+Ping, Hi, Hallo und YouTube :)
+Mehr Commands werden Kommen! -LG OmegaholzYT');
+  	}
+});
+
+client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Hi') {
+    	message.reply('Guten Tag!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Hallo') {
+    	message.reply('Guten Tag!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'YouTube') {
+    	message.reply('https://m.youtube.com/channel/UCTJQkBj3dhkgA1TXnpZGkhw');
   	}
 });
 
